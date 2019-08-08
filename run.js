a05a2e293055767043f939bbc2d29ff8c1a5aac6
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-08-08 16:11:57 +0100

    include date in file path and move to function

--- run.js
+++ run.js
@@ -1,14 +1,24 @@
 require('events').EventEmitter.defaultMaxListeners = 100
+
 const puppeteer = require('puppeteer');
 const fs = require('fs')
+
 const domain = 'https://www.heritagefund.org.uk/';
 const widths = [320, 480, 600, 800, 768, 1024, 1200]
 const paths = ['', 'funding', 'funding/outcomes']
+
+const buildFilePath = (path) => {
+    const isoDate = new Date().toISOString().split('T')[0]
+    return  `${isoDate}/${!path ? 'homepage' : path.replace('/', '-slash-')}`
+}
+
 for (const path of paths) {
     console.log('path is ' + path)
-    const filePath = !path ? 'homepage' : path
-    if (!fs.existsSync(__dirname + filePath)) {
-        fs.mkdirSync(__dirname + filePath, ({ recursive: true }))
+    const filePath = buildFilePath(path)
+    console.log(`file path is ${filePath}`)
+    const absoluteFilePath = `${__dirname}/${filePath}`
+    if (!fs.existsSync(absoluteFilePath)) {
+        fs.mkdirSync(absoluteFilePath, ({ recursive: true }))
     }
     for (const width of widths) {
         console.log(`width is ${width}`);
