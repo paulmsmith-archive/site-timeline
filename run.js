7b6c16b608f445131d02d9d79ce57b161ef35b6f
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-08-08 15:12:52 +0100

    fix homepage logic

--- run.js
+++ run.js
@@ -1,20 +1,19 @@
+require('events').EventEmitter.defaultMaxListeners = 100
 const puppeteer = require('puppeteer');
 const fs = require('fs')
 const domain = 'https://www.heritagefund.org.uk';
-widths = [320, 480, 600, 800, 768, 1024]
-paths = ['/funding']
+const widths = [320, 480, 600, 800, 768, 1024, 1200]
+const paths = ['/', '/funding']
 for (const path of paths) {
     console.log('path is ' + path)
-    const withHomePagePath = path === '/' ? 'homepage' : path
+    const withHomePagePath = path === '/' ? '/homepage' : path
     if (!fs.existsSync(__dirname + withHomePagePath)) {
         fs.mkdirSync(__dirname + withHomePagePath, ({ recursive: true }))
     }
     for (const width of widths) {
         console.log(`width is ${width}`);
         (async () => {
-            console.log(`width before screenshot is ${width}`)
             const browser = await puppeteer.launch();
-            console.log(`width before screenshot 2 is ${width}`)
             const page = await browser.newPage();
             await page.setViewport(Object.assign(page.viewport(), { width: parseInt(width) }));
             await page.goto(domain + path);
