eebca857dc544eac506e1a64d41df35f1b7df67f
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-08-08 15:41:20 +0100

    fixes

--- run.js
+++ run.js
@@ -3,12 +3,12 @@ const puppeteer = require('puppeteer');
 const fs = require('fs')
 const domain = 'https://www.heritagefund.org.uk';
 const widths = [320, 480, 600, 800, 768, 1024, 1200]
-const paths = ['/', '/funding']
+const paths = ['/', '/funding', '/funding/outcomes']
 for (const path of paths) {
     console.log('path is ' + path)
-    const withHomePagePath = path === '/' ? '/homepage' : path
-    if (!fs.existsSync(__dirname + withHomePagePath)) {
-        fs.mkdirSync(__dirname + withHomePagePath, ({ recursive: true }))
+    const filePath = path === '/' ? '/homepage' : path
+    if (!fs.existsSync(__dirname + filePath)) {
+        fs.mkdirSync(__dirname + filePath, ({ recursive: true }))
     }
     for (const width of widths) {
         console.log(`width is ${width}`);
@@ -17,9 +17,11 @@ for (const path of paths) {
             const page = await browser.newPage();
             await page.setViewport(Object.assign(page.viewport(), { width: parseInt(width) }));
             await page.goto(domain + path);
-            console.log('writing ' + `${withHomePagePath}/${width}.png`)
-            await page.screenshot({ path: `./${withHomePagePath}/${width}.png`, fullPage: true });
+            console.log('writing ' + `${filePath}/${width}.png`)
+            await page.screenshot({ path: `./${filePath}/${width}.png`, fullPage: true });
             await browser.close();
-        })()
+        })().catch((e) => {
+            console.log(`threw exception ${e}`)
+        })
     }
 };
