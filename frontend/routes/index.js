e0851f26db021a7695a74a06beffeaebcee1c3e7
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-08-27 11:53:51 +0100

    pass values through to date template

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const config = require('../config')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
-router.get('^/:date([0-9]{4}-[0-9]{2}-[0-9]{2})', function(req, res, next) {
-  res.render('date.html');
+router.get('^/:date([0-9]{4}-[0-9]{2}-[0-9]{2})', function(req, res) {
+  res.render('date.html', { widths: config.widths, pages: config.pages, date: req.params.date });
 });
 
 module.exports = router;
